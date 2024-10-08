63e765ba590b8a2c79cf2a664ddeadf6cb104b3f
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2024-10-08 20:27:38 +0600

    ingress attempt commit

--- mern-docker/server/app.js
+++ mern-docker/server/app.js
@@ -3,8 +3,20 @@ const app = express();
 
 import cors from 'cors';
 
+const allowedOrigins = [
+  'http://localhost:5173',       // For local development
+  'http://client:5173',          // For Kubernetes deployment
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
